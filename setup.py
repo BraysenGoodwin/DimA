c34c69c5e761b90fe9933e796ccd8eff473d6845
Yrish_mac <Yrish3@gmail.com>
2018-01-25 20:57:34 -0700

    Published

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from distutils.core import setup
 
-setup(name="DimA",version="0.1",packages=["DimA",],license="Open Source",long_description=open("README.txt").read())
+setup(name="DimA",version="0.1",packages=["DimA",],license="Open Source",long_description=open("README.txt").read(),author="Braysen Goodwin",description="Dimensional Analysis for Python",author_email="BraysenLilGoodwin@gmail.com")
